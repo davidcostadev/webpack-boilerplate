4c93c44834bc8e7e8baf7b0fd65809cfa73b520a
David Costa <davidcostadev@gmail.com>
2019-12-24 12:50:21 -0300

    Add examples of react with style

---
+++ src/ReactWithCSS.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './ReactWithSCSS.css';
+
+const App = () => (
+  <div className="App">
+    This is App
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('#root'));
